acd5ac0a39a0f8978cf0d79d90a63e6726293134
FlorianLepot <florian.lepot@gmail.com>
2015-01-06 15:47:31 +0100

    Update homepage

--- client/src/app/homepage/home.controller.js
+++ client/src/app/homepage/home.controller.js
@@ -3,5 +3,16 @@
 angular.module('organizat')
   .controller('HomeCtrl', function ($scope) {
 
-
+    $scope.user = {
+       title:     "Technical Program Manager",
+       email:     "ipsum@lorem.com",
+       firstName: "Naomi",
+       lastName:  "" ,
+       company:   "Google" ,
+       address:   "1600 Amphitheatre Pkwy" ,
+       city:      "Mountain View" ,
+       state:     "CA" ,
+       country:   "USA" ,
+       postalCode : "94043"
+    };
   });
