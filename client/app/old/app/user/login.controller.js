074bc4ce7e873a2b79bdc591b1296370ecee00ff
FlorianLepot <florian.lepot@gmail.com>
2015-01-24 00:13:13 +0100

    Remove old app & useless files

--- client/app/old/app/user/login.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('organizat')
-  .controller('LoginCtrl', function ($scope) {
-
-});
--- client/app/old/app/user/registration.controller.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('organizat')
-  .controller('RegistrationCtrl', function ($scope) {
-});
